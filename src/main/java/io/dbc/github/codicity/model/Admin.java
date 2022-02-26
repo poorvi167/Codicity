d2cc74fb407ebd34848a58f405a294c13a20c280
poorvi167 <poorvi.gla_cs.ccv19@gla.ac.in>
2022-02-26 14:44:32 +0530

    defined Admin class

---
+++ src/main/java/io/dbc/github/codicity/model/Admin.java
@@ -0,0 +1,13 @@
+/**
+ * User: Poorvi
+ * Date: 26-02-2022
+ * IDE: IntelliJ IDEA
+ * File: io.dbc.github.codicity.model.Admin.java
+ * JDK: Oracle JDK 17
+ */
+
+package io.dbc.github.codicity.model;
+
+public class Admin {
+
+}
